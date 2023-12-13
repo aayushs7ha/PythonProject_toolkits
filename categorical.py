13749725ea2b5afee02954e46136718b3ea57482
Aayush Sinha <aayush_sinha@live.com>
2023-12-13 14:42:53 +0530

    Update categorical.py

--- categorical.py
+++ categorical.py
@@ -4,8 +4,8 @@ print(categorical_columns)
 
 
 # Loop through categorical columns 
-for items in categorical_columns:
-  print(f"Value counts of {items} : \n{df[items].value_counts()}\n")
+for column in categorical_columns:
+  print(f"Value counts of {column} : \n{df[column].value_counts()}\n")
   # Plot count plot
   plt.figure(figsize=(10, 6))
   sns.countplot(x=column, data=df_service, order=df_service[column].value_counts().index)
